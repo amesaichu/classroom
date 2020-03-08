458289b40269442630124b8b79d16869c45fb856
Verdika Fajar Saputra <50388328+verdikaf@users.noreply.github.com>
2020-03-08 23:12:07 +0700

    auth firebase

--- app/src/main/java/com/amel/classroom/login.kt
+++ app/src/main/java/com/amel/classroom/login.kt
@@ -1,9 +1,14 @@
 package com.amel.classroom
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class login : AppCompatActivity() {
 
@@ -18,12 +23,54 @@ class login : AppCompatActivity() {
 
         b_login = findViewById(R.id.button_login)
         b_login.setOnClickListener{
-            startActivity(Intent(this, dashboard_user::class.java))
+            val email = input_id.text.toString()
+            val password = input_password.text.toString()
+            if (email.isEmpty()|| password.isEmpty()) {
+                Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+
+                    if (!it.isSuccessful){ return@addOnCompleteListener
+                        val intent = Intent (this, login::class.java)
+                        startActivity(intent)
+                    }
+                    else
+                        Toast.makeText(this, "Succesfully Login", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this, dashboard_user::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Failed Login: ${it.message}")
+                    Toast.makeText(this, "Email/Password incorrect", Toast.LENGTH_SHORT).show()
+                }
         }
 
         b_log_ad = findViewById(R.id.button_loginadmin)
         b_log_ad.setOnClickListener{
-            startActivity(Intent(this, dashboard_admin::class.java))
+            val email = input_id.text.toString()
+            val password = input_password.text.toString()
+            if (email.isEmpty()|| password.isEmpty()) {
+                Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+
+                    if (!it.isSuccessful){ return@addOnCompleteListener
+                        val intent = Intent (this, login::class.java)
+                        startActivity(intent)
+                    }
+                    else
+                        Toast.makeText(this, "Succesfully Login", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this, dashboard_admin::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Failed Login: ${it.message}")
+                    Toast.makeText(this, "Email/Password incorrect", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
