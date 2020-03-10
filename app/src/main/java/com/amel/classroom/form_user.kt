cf9c2ba0f16f7176d58359165dd09b60d3242783
danirosyad <sbi11dani@gmail.com>
2020-03-10 19:41:29 +0700

    ini baru

--- app/src/main/java/com/amel/classroom/form_user.kt
+++ app/src/main/java/com/amel/classroom/form_user.kt
@@ -2,11 +2,13 @@ package com.amel.classroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
 
 class form_user : AppCompatActivity() {
-
+    lateinit var ref : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_user)
+
     }
 }
